a4bc117d282cefbdcc07a26e00494f1070850662
GuiOliveiraSousa <guilherme.sousa184@etec.sp.gov.br>
2025-03-11 20:33:41 -0300

    aula ex 1
    aula exemplo 1

--- src/exemplos/ex-01/index.js
+++ src/exemplos/ex-01/index.js
@@ -5,7 +5,8 @@ import styles from './styles';
 function Exemplo1(){
     return(
         <View style={styles.container}>
-            <Text>Exemplo 1</Text>
+            <Text style={styles.título}>Exemplo 1</Text>
+            <Text style={styles.texto}>Aula de reactive-native com Expo</Text>
         </View>
     );
 
