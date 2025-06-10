515a4ffdf85e69e2a143f08cdfcafa86b638af1b
Guilherme De Oliveira Sousa <guilherme.sousa184@etec.sp.gov.br>
2025-06-10 20:36:52 -0300

    update

--- src/exemplos/index.js
+++ src/exemplos/index.js
@@ -1,40 +1,42 @@
 import { View, TouchableOpacity, Text } from "react-native";
+import styles from "./styles";
 
 export default function Exemplos() {
   return (
     
-    <View>
+    <View style={styles.container}>
+        <Text style={styles.titulo}>Exemplos</Text>
         <Text>Exemplos</Text>
 
-        <TouchableOpacity>
+        <TouchableOpacity style={styles.botao}>
             <Text>Exemplos 1</Text>
             </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity style={styles.botao}>
             <Text>Exemplos 2</Text>
             </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity style={styles.botao}>
             <Text>Exemplos 3</Text>
             </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity style={styles.botao}>
             <Text>Exemplos 4</Text>
             </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity style={styles.botao}>
             <Text>Exemplos 5</Text>
             </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity style={styles.botao}>
             <Text>Exemplos 6</Text>
             </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity style={styles.botao}>
             <Text>Exemplos 7</Text>
             </TouchableOpacity>
 
-        <TouchableOpacity>
+        <TouchableOpacity style={styles.botao}>
             <Text>Exemplos 8</Text>
             </TouchableOpacity>    
 
